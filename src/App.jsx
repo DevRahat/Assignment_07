9c8961d01670b80975f93fea66499c90a7340ea6
DevRahat <dev.rahatislam@gmail.com>
2024-03-17 21:35:36 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,124 @@
+import { useEffect, useState } from 'react'
+
+import './App.css'
+import SingleProduct from './SingleProduct';
+
+function App() {
+  const [products, setProducts]=useState([]);
+
+
+  useEffect(()=>{
+    fetch("./fakeData.json")
+    .then(res =>res.json())
+    .then(data => setProducts(data));
+  },[]);
+  const handleCart =(p) =>{
+    console.log(p);
+  }
+
+  return (
+    <>
+      
+      <div className='main-container1 mx-auto container'>
+
+        {/* NavBar start */}
+          <div className='navbar-Container mx-auto container'>
+          <div class="navbar bg-base-100">
+  <div class="flex-1">
+    <a class="btn btn-ghost text-xl">daisyUI</a>
+  </div>
+<div class="dropdown">
+<div class="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal pr-20">
+      <li><a>Home</a></li>
+      <li><a>Recipes</a></li>
+      <li><a>About</a></li>
+      <li><a>Search</a></li>
+    </ul>
+  </div>
+    </div>
+<div class="flex-none gap-2">
+    <div class="form-control">
+      <input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto rounded-3xl" />
+    </div>
+    <div class="dropdown dropdown-end">
+      <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+        <div class="w-10 rounded-full">
+          <img alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        </div>
+      </div>
+      <ul tabindex="0" class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+        <li>
+          <a class="justify-between">
+            Profile
+            <span class="badge">New</span>
+          </a>
+        </li>
+        <li><a>Settings</a></li>
+        <li><a>Logout</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+          </div>
+          {/* Navbar ends */}
+
+          {/* Banner start */}
+          <div className='Banner-section mx-auto container'>
+          <div class="hero min-h-screen bg-base-200">
+  <div class="hero-content text-center">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold text-white">Discover an exceptional cooking <br />class tailored for you</h1>
+      <p class="py-6">Basic Cooking basic hotel Management courses offers here. We provide International level of Hotel Management program all over the world</p>
+      <button class="btn bg-green-600 border rounded-3xl">Explore Now</button>
+      <button class="btn bg-white ml-5 border rounded-3xl">Our Feedback</button>
+    </div>
+  </div>
+</div>
+          </div>
+          {/* Banner section ends */}
+
+          {/* Main Body start */}
+          <div className='main-container2'>
+          <h2 className='Body-headings text-center font-extrabold'>Our Recipes</h2>
+          <p className='body-text text-center'>Have a look to all of our Recipes & chose your's fav one. <br></br> We are always at your service </p>
+
+          <div className='main-container'>
+            <div className='cards-container'>
+
+            {
+              products.map((pd) =>(<SingleProduct product={pd} handleCart={handleCart }></SingleProduct>
+              ))}
+
+
+          
+            </div>
+            <div className='cart-container'>
+                <div className='cart-title'>
+                  <h5>
+                    Name
+                  </h5>
+                  <h5>
+                    Price
+                  </h5>
+                </div>
+            </div>
+          </div>
+          
+
+
+
+          </div>
+          {/* main Body ends Here */}
+
+
+
+
+      </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/SingleProduct.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const SingleProduct = ({product, handleCart }) => {
+    console.log(handleCart);
+    return (
+        <div>
+            <div class="card w-96 bg-base-100 shadow-xl text-start ">
+                <figure class="px-10 pt-10">
+                <img src={product.recipe_image} alt="Shoes" class="rounded-xl" />
+                </figure>
+            <div class="card-body  text-left text-gray-500">
+            <h2 class="card-title">{product.recipe_name}</h2>
+            <p>{product.short_description}</p>
+             <hr />
+             <div className='Ingredients'>
+                 <ol className='text-left'>
+                    <h4>Ingredients: 6</h4>
+                     <li>1</li>
+                 </ol>
+                </div>
+        <div className='time flex gap-4 text-gray-500 text-left'>
+            <p>{product.preparing_time}</p>
+            <p>{product.calories}</p>
+        </div>
+
+    <div class="card-actions">
+      <button onClick={(e) =>handleCart(product)} class="btn bg-green-500 text-black border rounded-full">Cook Now</button>
+    </div>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default SingleProduct;
